5340a0bde0d21cf4d4051c675ff3ff24af1da019
kristina <krisbubenka@icloud.com>
2024-09-17 21:01:21 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+/** @format */
+
+function check(str) {
+  let stackStr = [];
+  console.log(str);
+  for (let i = 0; i < str.length; i++) {
+    if (str.length == 0) {
+      return false;
+    }
+    if (str[i] == "(" || str[i] == "{" || str[i] == "[") {
+      stackStr.push(str[i]);
+    } else if (
+      str[i] == ")" &&
+      stackStr[stackStr.length - 1] == "(" &&
+      stackStr.length > 0
+    ) {
+      stackStr.pop();
+    } else if (
+      str[i] == "}" &&
+      stackStr[stackStr.length - 1] == "{" &&
+      stackStr.length > 0
+    ) {
+      stackStr.pop();
+    } else if (
+      str[i] == "]" &&
+      stackStr[stackStr.length - 1] == "[" &&
+      stackStr.length > 0
+    ) {
+      stackStr.pop();
+    } else if (
+      (str[i] == ")" || str[i] == "}" || str[i] == "]") &&
+      stackStr.length === 0
+    ) {
+      return false;
+    }
+  }
+  console.log(stackStr);
+  return stackStr.length === 0;
 }
+
+let res = check("");
+console.log(res);
+
+module.exports = check;
